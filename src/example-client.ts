fc74531c247b4784067c398d2f52e89474fec97c
Nufayl Razick <nufayl@gmail.com>
2024-03-24 13:11:32 +0000

    refactor: removed legacy BookSearchApiClient and example client code

--- src/example-client.ts
+++ src/example-client.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { z } from "zod";
+import { createBookSearchApiClient } from "./BookSearchApiClient";
+
 // Define the schema for the book data
 const bookAuthorSchema = z.object({
   id: z.number(),
@@ -16,3 +18,15 @@ const axiosInstance = axios.create({
   baseURL: "https://book-seller.free.beeceptor.com",
 });
 const authorSearchQuery = `/by-author?authorName=jkr&limit=10&format=json`;
+
+//https://book-seller.free.beeceptor.com/by-author?authorName=jkr&limit=10&format=json
+const bookSearchApiClient = createBookSearchApiClient(
+  bookAuthorSchema,
+  axiosInstance
+);
+
+bookSearchApiClient
+  .getBooksByAuthor<Book>(authorSearchQuery)
+  .then((booksByAuthor) => {
+    console.log("Books by author:", booksByAuthor);
+  });
